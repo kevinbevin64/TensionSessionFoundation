b814d755a604dd8ce261c653819df71bfdb9519d
Kevin Chen <73233100+kevinbevin64@users.noreply.github.com>
2026-09-23 18:23:58 -0500

    Add WatchTransferring type

---
+++ Sources/TensionSessionFoundation/Types/Watch Transferring/WatchTransferring+Error.swift
@@ -0,0 +1,14 @@
+//
+//  WatchTransferring+Error.swift
+//  TensionSession
+//
+//  Created by Kevin Chen on 8/23/26.
+//
+
+extension WatchTransferring {
+    
+    enum Error: Swift.Error {
+        
+        case notDictionary
+    }
+}
---
+++ Sources/TensionSessionFoundation/Types/Watch Transferring/WatchTransferring.swift
@@ -0,0 +1,8 @@
+//
+//  WatchTransferring.swift
+//  TensionSession
+//
+//  Created by Kevin Chen on 8/23/26.
+//
+
+enum WatchTransferring {}
